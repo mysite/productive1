c1c4a12867af9e9bf0db2b943491459bd9101a27
Fabian Gatzka <Fabian@Gatzka-online.de>
2020-11-25 23:32:46 +0100

    Version 2.0 final

--- polls/admin.py
+++ polls/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
 
-<<<<<<< HEAD
 from .models import Contact, Newsletter#, Pictures
-=======
-from .models import Contact #, Pictures
->>>>>>> 077e043e98c2e6964d96bb90fe3f0e59a5e4307f
 
 #admin.site.register(Picture)
 #class PictureAdmin(admin.ModelAdmin):
@@ -15,10 +11,7 @@ from .models import Contact #, Pictures
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'subject','text')
 
-<<<<<<< HEAD
+
 admin.site.register(Contact, ContactAdmin)
 
 admin.site.register(Newsletter)
-=======
-admin.site.register(Contact, ContactAdmin)
->>>>>>> 077e043e98c2e6964d96bb90fe3f0e59a5e4307f
